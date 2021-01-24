d08466c4bc8536963d34aa3b262dc28114141c6f
David Gatica <davgatik@gmail.com>
2021-01-24 03:13:00 -0600

    Ciclo For

---
+++ for.py
@@ -0,0 +1,20 @@
+# imprimir los numeros del uno al mil
+
+contador = 1
+
+# print(contador)
+# while contador < 1000:
+# 	#contador = contador + 1 
+# 	contador += 1 # contador++;
+# 	print(contador)
+
+#a = range(1000)  #rango de cero a mil
+#print(a)
+
+for contador in range(1, 1001): # el valor final siempre llega a una posicion anterior
+	print(contador)
+
+for i in range(11):
+	print(2 * i)
+
+

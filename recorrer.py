e4d4dcfad3a7826bf378d9e259275d41c69ff77b
David Gatica <davgatik@gmail.com>
2021-01-25 22:50:29 -0600

    Recorrer una Cadena de Caracteres

---
+++ recorrer.py
@@ -0,0 +1,23 @@
+##Recorrer
+def run():
+	# recorriendo un string
+	# nombre = input('Ingresa tu nombre: ')
+	# for letra in range(len(nombre)):
+	# 	print(nombre[letra])
+
+	# for letra in nombre:
+	# 	print(letra)
+	# recorrer un string y pasarlo a Mayusculas
+	frase = input('Escribe una Frase: ')
+	for caracter in frase:
+		print(caracter.upper())
+
+
+
+
+
+
+
+# entry point
+if __name__ == '__main__':
+	run()

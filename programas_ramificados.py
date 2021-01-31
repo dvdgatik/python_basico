431c5ac528478e34b42506e9b5089647a19a35f9
David Gatica <davgatik@gmail.com>
2021-01-31 00:12:31 -0600

    Programas Ramificados David Aro

---
+++ programas_ramificados.py
@@ -0,0 +1,10 @@
+# dar dos enteros y compararlos
+num_1 = int(input('Escoge un entero: '))
+num_2 = int(input('Escoge otro entero: '))
+
+if num_1 > num_2:
+	print('El primer numero  es mayor que el segundo')
+elif num_1 < num_2:
+	print('EL segundo numero es mayor que el primero')
+else: 
+	print('Los dos numeros son iguales')
